c528da5633deedc25bfb7e84b09b44c08f31ebb3
Davide Terzani <titoiride@gmail.com>
2022-04-11 15:53:48 -0700

    Add ASCII file reader

---
+++ include/ReadASCII.hpp
@@ -0,0 +1,34 @@
+/* Copyright 2022 Davide Terzani
+ *
+ * This file is part of GRAPPA.
+ *
+ * License: BSD-3-Clause
+ */
+
+#pragma once
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <G4ParticleMomentum.hh>
+#include <G4ThreeVector.hh>
+
+typedef std::pair<G4ThreeVector, G4ParticleMomentum> GRAPPAParticle;
+class ASCIIReader
+{
+private:
+    std::string m_filename;
+    std::ifstream m_filestream;
+    std::shared_ptr<std::vector<GRAPPAParticle>> m_particlecollection;
+
+public:
+    ASCIIReader();
+    ASCIIReader(std::string filename);
+    ~ASCIIReader();
+    G4int CountParticles();
+    void ReadParticles();
+    inline std::shared_ptr<std::vector<GRAPPAParticle>> GetParticles() { return m_particlecollection; };
+    inline std::string GetFileName() { return m_filename; };
+};
---
+++ src/ReadASCII.cc
@@ -0,0 +1,74 @@
+/* Copyright 2022 Davide Terzani
+ *
+ * This file is part of GRAPPA.
+ *
+ * License: BSD-3-Clause
+ */
+
+#include <ReadASCII.hpp>
+
+ASCIIReader::ASCIIReader()
+{
+    m_particlecollection = std::make_shared<std::vector<GRAPPAParticle>>();
+}
+
+ASCIIReader::ASCIIReader(std::string filename)
+{
+    m_filename = filename;
+    m_filestream.open(m_filename);
+    if (!m_filestream.is_open())
+    {
+        G4ExceptionDescription msg;
+        msg << "Impossible to open " << m_filename;
+        msg << " Please check if file exists and is accessible";
+        G4Exception("ASCIIReader::ASCIIReader()",
+                    "GRAPPA::FILE_NOT_OPEN", FatalException, msg);
+    }
+    m_particlecollection = std::make_shared<std::vector<GRAPPAParticle>>();
+}
+
+ASCIIReader::~ASCIIReader()
+{
+    if (m_filestream.is_open())
+    {
+        m_filestream.close();
+    }
+}
+
+G4int ASCIIReader::CountParticles()
+{
+    G4int nlines = 0;
+    std::string dumpline;
+
+    while (m_filestream.good())
+    {
+        std::getline(m_filestream, dumpline);
+        nlines++;
+    }
+    m_filestream.clear();
+    m_filestream.seekg(0);
+    return nlines;
+}
+
+void ASCIIReader::ReadParticles()
+{
+    double x, y, z, px, py, pz;
+    G4ThreeVector nextposition;
+    G4ParticleMomentum nextmomentum;
+    GRAPPAParticle nextparticle;
+    while (m_filestream.good())
+    {
+        m_filestream >> x;
+        m_filestream >> y;
+        m_filestream >> z;
+        m_filestream >> px;
+        m_filestream >> py;
+        m_filestream >> pz;
+
+        nextposition = G4ThreeVector(x, y, z);
+        nextmomentum = G4ParticleMomentum(px, py, pz);
+
+        nextparticle = {nextposition, nextmomentum};
+        m_particlecollection->push_back(nextparticle);
+    }
+}
