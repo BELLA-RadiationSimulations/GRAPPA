814b129d6a24946113459e169e90fda5893745fc
Davide Terzani <23354403+titoiride@users.noreply.github.com>
2025-04-08 09:41:36 -0700

    clang-formatted

--- src/GRPParticleContainer.cpp
+++ src/GRPParticleContainer.cpp
@@ -38,8 +38,7 @@ void GRPParticleContainer::DefineCommands()
     m_CMDpartsfromfile = std::make_shared<G4GenericMessenger>(
         this, "/particlesource/", "Set if GRAPPA reads particles from file.");
 
-    G4GenericMessenger::Command ifpartsfromfile, setpart, filename,
-        list;
+    G4GenericMessenger::Command ifpartsfromfile, setpart, filename, list;
 
     ifpartsfromfile = m_CMDpartsfromfile->DeclareProperty(
         "particlesFromFile", m_useFile, "Read particles from a file.");
