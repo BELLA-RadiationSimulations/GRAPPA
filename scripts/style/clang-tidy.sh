cd6ac0ed34b6e2de430f56c4ec175d6b7e24289e
Davide Terzani <23354403+titoiride@users.noreply.github.com>
2026-09-21 11:28:56 -0700

    Updated workflow

--- scripts/style/clang-tidy.sh
+++ scripts/style/clang-tidy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export CXX=$(which clang)
-export CC=$(which clang)
-
 cmake -S . -B build \
   -DCMAKE_PREFIX_PATH="$HOME/anaconda3/envs/geant4/lib/" \
   -DCMAKE_CXX_CLANG_TIDY="clang-tidy;--system-headers=0;--config-file=$PWD/.clang-tidy"
